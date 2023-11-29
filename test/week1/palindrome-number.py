a6d69af39883e625f3b9d64463634d6cac3a6400
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2023-11-29 19:27:12 +0300

    Added solution for Palindrome Number

---
+++ test/week1/palindrome-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        rev = 0
+        num = x
+        while x > 0:
+            rev = rev * 10  + (x % 10)
+            x = x // 10
+        return num == rev
