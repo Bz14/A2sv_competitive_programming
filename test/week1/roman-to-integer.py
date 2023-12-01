0595c893fc5c18c3d67a06605ddff6a068ca5324
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2023-12-01 19:25:32 +0300

    Added solution for Roman to Integer

---
+++ test/week1/roman-to-integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_dict = {"I": 1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        spec = {"IV":4, "IX":9, "XL":40, "XC":90, "CD":400, "CM":900}
+        res = 0
+        i = 0
+        while i < len(s):
+            if s[i: i + 2] in spec:
+                res +=  spec[s[i:i + 2]]
+                i += 2
+            else:
+                res += roman_dict[s[i]]
+                i += 1
+        return res
+
+
+
+        
