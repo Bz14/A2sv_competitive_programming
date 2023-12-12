2f63cf5a6ada32441c8732fba6f798acecf39c31
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2023-12-12 13:01:33 +0300

    Added solution for Two Sum

--- test/week1/two-sum.py
+++ test/week1/two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sumdict = dict()
+        numdict = dict()
         for idx, num in enumerate(nums):
             diff = target - num
-            if diff in sumdict:
-                return [idx, sumdict[diff]]
-            sumdict[num] = idx
+            if diff in numdict:
+                return [idx, numdict[diff]]
+            numdict[num] = idx
