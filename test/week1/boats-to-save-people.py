835fccd7a34f54a016948fdb2a8bc1aa7c865a62
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-01-03 19:41:19 +0300

    Added solution for Boats to Save People

---
+++ test/week1/boats-to-save-people.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        l = 0
+        r = len(people) - 1
+        minBoat = 0
+        while l < r:
+            if people[l] + people[r] <= limit:
+                l += 1
+                r -= 1
+                minBoat += 1
+            else:
+                minBoat += 1
+                r -= 1
+        if l == r:
+            minBoat += 1
+        return minBoat
+
