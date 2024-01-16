c1834a5445b2fabf037234480ae50c783dd18781
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-01-16 23:32:04 +0300

    Add solution for Range Sum Query - Immutable

--- test/week1/leetcode/range-sum-query-immutable.py
+++ test/week1/leetcode/range-sum-query-immutable.py
@@ -9,8 +9,6 @@ class NumArray:
     def sumRange(self, left: int, right: int) -> int:
         return self.prefix_sum[right + 1] - self.prefix_sum[left]
         
-
-
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
