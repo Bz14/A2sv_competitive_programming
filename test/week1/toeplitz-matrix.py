181bb43bd449f2ccb767fafe6410c7d2115f955f
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-01-19 13:10:26 +0300

    Added solution for Toeplitz Matrix

--- test/week1/toeplitz-matrix.py
+++ test/week1/toeplitz-matrix.py
@@ -1,21 +1,7 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        for i in range(1,len(matrix)):
-            for j in range(1, len(matrix[i])):
-                if matrix[i][j] != matrix[i-1][j-1]:
+        for i in range(len(matrix) - 1):
+            for j in range(len(matrix[i]) - 1):
+                if matrix[i][j] != matrix[i+1][j+1]:
                     return False
         return True
-
-        
-        """
-        1 2 3 4
-        5 1 2 3
-        9 5 1 2
-
-        13 24
-
-        1 1 1 -> 00 11 22
-        2 2 2 -> 01 12 23
-        3 3 -> 02 13
-        4 -> 03
-        """
