6de859bb70e413f953c0284a662773a30c23f87f
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-01-19 15:06:09 +0300

    Added solution for Number of Good Pairs

--- test/week1/number-of-good-pairs.py
+++ test/week1/number-of-good-pairs.py
@@ -1,8 +1,7 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        count = 0
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] == nums[j]:
-                    count += 1
-        return count
+        freq_count = Counter(nums)
+        result = 0
+        for num, count in freq_count.items():
+            result += (count * (count - 1)) // 2
+        return result
