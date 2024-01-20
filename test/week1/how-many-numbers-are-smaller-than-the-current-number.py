ac1825f4199df72e9c3525c886ed691ce45e1cf0
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-01-21 00:00:23 +0300

    Added solution for How Many Numbers Are Smaller Than the Current Number

--- test/week1/how-many-numbers-are-smaller-than-the-current-number.py
+++ test/week1/how-many-numbers-are-smaller-than-the-current-number.py
@@ -4,10 +4,7 @@ class Solution:
         valDict = defaultdict(list)
         for i, num in enumerate(nums):
             valDict[num].append(i)
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] > nums[j]:
-                    nums[i], nums[j] = nums[j], nums[i]
+        nums.sort()
         count = 0
         for i in range(len(nums)):
             if i > 0 and nums[i] != nums[i - 1]:
