67b52ecea57a2f119641b1b13c3d5c0d3276c0ea
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-01-21 17:39:58 +0300

    Added solution for Minimum Size Subarray Sum

--- test/week1/minimum-size-subarray-sum.py
+++ test/week1/minimum-size-subarray-sum.py
@@ -4,11 +4,10 @@ class Solution:
         j = 0
         total = 0
         minLen = len(nums) + 1
-        while i < len(nums):
+        for i in range(len(nums)):
             total += nums[i]
             while total >= target:
                 minLen = min(minLen, i - j + 1)
                 total -= nums[j]
                 j += 1
-            i += 1
         return minLen % (len(nums) + 1)
