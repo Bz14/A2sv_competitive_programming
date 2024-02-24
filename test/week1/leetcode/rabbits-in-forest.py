401b3c5fe34abbdd24074f6b4fd3bea30cd42601
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-02-24 08:48:15 +0300

    Add solution for Rabbits in Forest

--- test/week1/leetcode/rabbits-in-forest.py
+++ test/week1/leetcode/rabbits-in-forest.py
@@ -3,10 +3,6 @@ class Solution:
         count = Counter(answers)
         res = 0
         for key, value in count.items():
-            if key == 0:
-                res += value
-            elif key + 1 >= value:
-                res += key + 1
-            else:
-                res += (ceil(value / (key + 1)) * (key + 1))
+            numberOfPairs = ceil(value / (key + 1))
+            res += numberOfPairs * (key + 1)
         return res
