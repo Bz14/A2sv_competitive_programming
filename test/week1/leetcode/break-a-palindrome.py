141df89c3c2b27d21f157fe2ab68b35cbfaa6e30
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-02-25 12:24:25 +0300

    Add solution for Break a Palindrome

--- test/week1/leetcode/break-a-palindrome.py
+++ test/week1/leetcode/break-a-palindrome.py
@@ -4,11 +4,10 @@ class Solution:
         if len(palindrome) == 1:
             return ''
         palindrome = list(palindrome)
-        for i in range(len(palindrome)):
-            if count['a'] == len(palindrome) - 1:
-                continue
-            if palindrome[i] != 'a':
-                palindrome[i] = 'a'
-                return ''.join(palindrome)
+        if count['a'] != len(palindrome) - 1:
+            for i in range(len(palindrome)):
+                if palindrome[i] != 'a':
+                    palindrome[i] = 'a'
+                    return ''.join(palindrome)
         palindrome[-1] = 'b'
         return ''.join(palindrome)
