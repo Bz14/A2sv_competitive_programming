4970a6b31c0329c0a0207d61e89fb57227dfc555
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-03-07 12:33:28 +0300

    Add solution for Heaters

--- test/week1/leetcode/heaters.py
+++ test/week1/leetcode/heaters.py
@@ -14,13 +14,11 @@ class Solution:
 
         l = 0
         r = max(max(heaters),max(houses))
-        ans = max(max(heaters),max(houses))
         mid = 0
         while l <= r:
             mid = (l + r) // 2
             if check(mid):
-                ans = min(ans, mid)
                 r = mid - 1
             else:
                 l = mid + 1
-        return ans
+        return l
