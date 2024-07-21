d362e50109221c8cbf72525c955a73e85b8ea0e7
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-07-21 21:10:16 +0300

    Created 'Number of Wonderful Substrings' by A2SV Hub

---
+++ 21-Jul-2024/Number of Wonderful Substrings 182578.py
@@ -0,0 +1,16 @@
+# Problem: Number of Wonderful Substrings - https://leetcode.com/problems/number-of-wonderful-substrings/
+
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+        prefix_map = defaultdict(int)
+        prefix_map[0] = 1
+        ans = 0
+        mask = 0
+        for w in word:
+            mask ^= 1 << (ord(w) - ord('a'))
+            ans += prefix_map[mask]
+            for i in range(10):
+                pre_mask = mask ^ (1 << i)
+                ans += prefix_map[pre_mask]
+            prefix_map[mask] += 1
+        return ans
