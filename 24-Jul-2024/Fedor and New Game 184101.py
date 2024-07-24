0137a34c43d08005fa9179ad2731b176f54ac9c2
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-07-24 12:39:23 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 24-Jul-2024/Fedor and New Game 184101.py
@@ -0,0 +1,27 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n, m, k = list(map(int, input().split()))
+arr = []
+for _ in range(m + 1):
+    arr.append(int(input()))
+player1 = arr[-1]
+ans = 0
+for i in range(m):
+    player2 = arr[i]
+    maxVal = max(player1, player2)
+    val = 1
+    idx = 0
+    count = 0
+    while val <= maxVal:
+        val = 1 << idx
+        if player1 & val != 0 and player2 & val == 0:
+            count += 1
+        if player1 & val == 0 and player2 & val != 0:
+            count += 1
+        idx += 1
+    if count <= k:
+        ans += 1
+print(ans)
+
+        
+
