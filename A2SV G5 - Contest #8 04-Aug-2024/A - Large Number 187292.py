f205898a3f8043612c09cca8c6ca88ef546c7c40
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-08-04 20:50:09 +0300

    Created 'A - Large Number' by A2SV Hub

---
+++ A2SV G5 - Contest #8 04-Aug-2024/A - Large Number 187292.py
@@ -0,0 +1,12 @@
+# Problem: A - Large Number - https://codeforces.com/gym/511242/problem/A
+
+testcase = int(input())
+for _ in range(testcase):
+    n, d = list(map(int, input().split()))
+    num = input()
+    numStr = str(num)
+    idx = 0
+    while idx < n and int(numStr[idx]) >= d:
+        idx += 1
+    ans = numStr[:idx] + str(d) + numStr[idx:]
+    print(ans)
