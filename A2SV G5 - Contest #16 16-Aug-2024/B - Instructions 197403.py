b2b7b53a6e897a83f30b717d9ef5fdce3d2f3483
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-08-17 00:14:21 +0300

    Created 'B - Instructions' by A2SV Hub

---
+++ A2SV G5 - Contest #16 16-Aug-2024/B - Instructions 197403.py
@@ -0,0 +1,14 @@
+# Problem: B - Instructions - https://codeforces.com/gym/523525/problem/B
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        dp = arr.copy()
+        for i in range(n):
+            if i + arr[i] < n:
+                dp[i + arr[i]] = max(dp[i + arr[i]], dp[i] + arr[i + arr[i]])
+        print(max(dp))
+
+main()
