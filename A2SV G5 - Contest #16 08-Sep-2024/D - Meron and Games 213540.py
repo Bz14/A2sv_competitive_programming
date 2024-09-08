818dbf0dbd3790c29a49d3db41bb58f3080ce74d
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-09-08 18:34:50 +0300

    Created 'D - Meron and Games' by A2SV Hub

---
+++ A2SV G5 - Contest #16 08-Sep-2024/D - Meron and Games 213540.py
@@ -0,0 +1,28 @@
+# Problem: D - Meron and Games - https://codeforces.com/gym/523525/problem/D
+
+import sys, threading
+from collections import Counter
+def main():
+    n = int(input())
+    arr = list(map(int, input().split()))
+    memo = {}
+    count = Counter(arr)
+    maxVal = max(arr)
+
+    def dp(num):
+        if num in memo:
+            return memo[num]
+        if num > maxVal:
+            return 0
+        val = max(num * count[num] + dp(num + 2), dp(num + 1))
+        memo[num] = val
+        return memo[num]
+    print(dp(min(arr)))
+        
+if __name__ == '__main__':
+    sys.setrecursionlimit(1 << 30)
+    threading.stack_size(1 << 27)
+ 
+    main_thread = threading.Thread(target=main)
+    main_thread.start()
+    main_thread.join()
