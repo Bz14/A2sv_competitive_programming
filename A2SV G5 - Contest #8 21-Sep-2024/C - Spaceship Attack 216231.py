9bf968cabfe4edfd354ba89c6504130d7b9260d3
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-09-21 21:51:00 +0300

    Created 'C - Spaceship Attack' by A2SV Hub

---
+++ A2SV G5 - Contest #8 21-Sep-2024/C - Spaceship Attack 216231.py
@@ -0,0 +1,24 @@
+# Problem: C - Spaceship Attack - https://codeforces.com/gym/511242/problem/C
+
+n, m = list(map(int, input().split()))
+spaceship = list(map(int, input().split()))
+bases = []
+for _ in range(m):
+    value = list(map(int, input().split()))
+    bases.append(value)
+bases.sort()
+pre_sum = [0]
+for i in range(m):
+    pre_sum.append(pre_sum[-1] + bases[i][1])
+ans = []
+for s in spaceship:
+    l = 0
+    r = len(bases) - 1
+    while l <= r:
+        mid = (l + r) // 2
+        if bases[mid][0] <= s:
+            l = mid + 1
+        else:
+            r = mid - 1
+    ans.append(pre_sum[r + 1])
+print(*ans)
