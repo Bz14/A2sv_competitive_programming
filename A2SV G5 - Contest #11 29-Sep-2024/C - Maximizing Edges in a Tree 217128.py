12cc0e6e837af5336c5ce0a99afd39628bfbf6c1
Eyerusalem Bezu <108490577+Bz14@users.noreply.github.com>
2024-09-29 20:11:25 +0300

    Created 'C - Maximizing Edges in a Tree' by A2SV Hub

---
+++ A2SV G5 - Contest #11 29-Sep-2024/C - Maximizing Edges in a Tree 217128.py
@@ -0,0 +1,25 @@
+# Problem: C - Maximizing Edges in a Tree - https://codeforces.com/gym/515998/problem/C
+
+from collections import defaultdict
+n = int(input())
+graph = defaultdict(list)
+for _ in range(n - 1):
+    u, v = list(map(int, input().split()))
+    graph[u].append(v)
+    graph[v].append(u)
+n = len(graph)
+colors = [-1 for _ in range(n + 1)]
+colors[1]  = 0
+stack = [1]
+while stack:
+    node = stack.pop()
+    for vertex in graph[node]:
+        if colors[vertex] == -1:
+            if colors[node] == 0:
+                colors[vertex] = 1
+            else:
+                colors[vertex] = 0
+            stack.append(vertex)
+numZeros = colors.count(0)
+numOnes = colors.count(1)
+print(numOnes * numZeros - (n - 1))
